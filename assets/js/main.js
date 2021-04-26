8701b54c1659a4cf90c378ae3e648af1c0f5f384
lumationDevelopers <cristianfergmz@gmail.com>
2021-04-25 22:55:03 -0600

    projects dinamics and added technologies skill

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,21 @@
 * License: https://bootstrapmade.com/license/
 */
 (function($) {
+  /* var search = location.search.substring(1);
+  const jsonGetParams = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+  var project = jsonGetParams.landing_country; */  
+
+      
+{/* <div class="item Webapps col-sm-6 col-md-6 col-lg-6 mb-4">
+            <a href="quick-box.html" class="item-wrap fancybox">
+              <div class="work-info">
+                <h3>Quick box</h3>
+                <p>Website</p>
+              </div>
+              <img class="img-fluid" src="assets/img/quickbox.png">
+            </a>
+          </div> */}
+
   "use strict";
 
   var burgerMenu = function() {
@@ -18,7 +33,7 @@
   }
   burgerMenu();
 
-  var siteIstotope = function() {
+  /* var siteIstotope = function() {
     var $container = $('#portfolio-grid').isotope({
       itemSelector: '.item',
       isFitWidth: true
@@ -34,19 +49,19 @@
       filter: '*'
     });
 
-    $('#filters').on('click', 'a', function(e) {
+    $('#portfolio-flters').on('click', 'a', function(e) {
       e.preventDefault();
       var filterValue = $(this).attr('data-filter');
       $container.isotope({
         filter: filterValue
       });
-      $('#filters a').removeClass('active');
+      $('#portfolio-flters a').removeClass('active');
       $(this).addClass('active');
     });
   }
   $(window).on('load', function() {
     siteIstotope();
-  });
+  }); */
 
   var siteOwlCarousel = function() {
     $('.testimonial-carousel').owlCarousel({
@@ -67,13 +82,7 @@
       once: true
     });
   });
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+
   // Skills section
   $('.skills-content').waypoint(function() {
     $('.progress .progress-bar').each(function() {
@@ -82,4 +91,62 @@
   }, {
     offset: '80%'
   });
+    var project;
+  
+
+    var siteIstotope = function() {
+      var $container = $('#portfolio-grid').isotope({
+        itemSelector: '.item',
+        isFitWidth: true
+      });
+  
+      $(window).resize(function() {
+        $container.isotope({
+          columnWidth: '.col-sm-3'
+        });
+      });
+  
+      $container.isotope({
+        filter: '*'
+      });
+  
+      $('#portfolio-flters').on('click', 'a', function(e) {
+        e.preventDefault();
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({
+          filter: filterValue
+        });
+        $('#portfolio-flters a').removeClass('active');
+        $(this).addClass('active');
+      });
+    }
+/*     $(window).on('load', function() {
+      siteIstotope();
+    }); */
+  fetch("https://api.jsonbin.io/b/608516b15210f622be393032", {
+    method: 'GET', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  })
+      .then(response => response.json())
+      .then(jsonLocal => {
+          project = jsonLocal;
+          project.forEach(project_new => {
+            $("#portfolio-grid").append(`
+            <div class="item web-app col-sm-6 col-md-6 col-lg-6 mb-4">
+              <a href="work-single.html?name=${project_new.name}" class="item-wrap fancybox">
+                <div class="work-info">
+                  <h3>${project_new.name}</h3>
+                  <p>${project_new.category}</p>
+                </div>
+                <img class="img-fluid" src="assets/img/${project_new.image_url}">
+              </a>
+            </div>`)
+          })
+
+        })
+        .catch(error => {
+          console.log("error")
+      })
 })(jQuery);
